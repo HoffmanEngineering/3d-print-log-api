116a1b41d214f360281d349c13c17c7c7cd80056
Christopher Hoffman <CSH.Hoffman@gmail.com>
2021-01-29 18:15:57 -0500

    Fixed the filament usage on the CVS Export and the Analytics pages.

--- PrintLogApi/Profiles/PrintProfile.cs
+++ PrintLogApi/Profiles/PrintProfile.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using PrintLogApi.Models;
 using PrintLogApi.Models.DTOs.Print;
 
@@ -30,12 +31,30 @@ namespace PrintLogApi.Profiles
                 .ForMember(dest => dest.Images, opt => opt.Ignore())
                 .ForMember(dest => dest.Comments, opt => opt.Ignore());
 
-            CreateMap<Print, PrintStatistic>();
+            CreateMap<Print, PrintStatistic>()
+                .ForMember(dest => dest.EstimatedFilamentUsageMg, opt => opt.MapFrom(src => src.FilamentUsage.Sum(p => p.EstimatedAmountMg.HasValue &&
+                                                                                                                    p.EstimatedAmountMg > 0 ?
+                                                                                                                    p.EstimatedAmountMg : 0)
+                                                                                            + (src.EstimatedFilamentUsageMg ?? 0)))
+                .ForMember(dest => dest.FilamentUsageMg, opt => opt.MapFrom(src => src.FilamentUsage.Sum(p => p.AmountMg.HasValue &&
+                                                                                                                    p.AmountMg > 0 ?
+                                                                                                                    p.AmountMg : 0)
+                                                                                            + (src.FilamentUsageMg ?? 0)));
 
             CreateMap<Print, PrintDetailReport>()
                 .ForMember(dest => dest.PrinterName, opt => opt.MapFrom(src => src.Printer.Name))
                 .ForMember(dest => dest.PrinterMake, opt => opt.MapFrom(src => src.Printer.Make))
-                .ForMember(dest => dest.PrinterModel, opt => opt.MapFrom(src => src.Printer.Model));
+                .ForMember(dest => dest.PrinterModel, opt => opt.MapFrom(src => src.Printer.Model))
+                .ForMember(dest => dest.EstimatedFilamentUsageG, opt => opt.MapFrom(src => (src.FilamentUsage.Sum(p => p.EstimatedAmountMg.HasValue && 
+                                                                                                                    p.EstimatedAmountMg > 0 ?
+                                                                                                                    p.EstimatedAmountMg : 0) 
+                                                                                            + (src.EstimatedFilamentUsageMg ?? 0))/1000.0))
+                .ForMember(dest => dest.FilamentUsageG, opt => opt.MapFrom(src => (src.FilamentUsage.Sum(p => p.AmountMg.HasValue &&
+                                                                                                                    p.AmountMg > 0 ?
+                                                                                                                    p.AmountMg : 0)
+                                                                                            + (src.FilamentUsageMg ?? 0))/1000.0))
+                // Combine the FilamentTypes with the display names of the filaments, deliminated by ;.
+                .ForMember(dest => dest.FilamentType, opt => opt.MapFrom(src => (src.FilamentType + "; " + string.Join("; ", src.FilamentUsage.Select(f => f.Filament.DisplayName).ToList())).Trim().Trim(';').Trim()));
 
             CreateMap<PrintFilament, PrintFilamenSummaryDto>()
                 .ForMember(dest => dest.Filament, opt => opt.MapFrom(src => src.Filament));
