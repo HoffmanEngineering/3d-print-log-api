2805ef5a3dd624b2ad82f1cb774d9c5458aa2122
Christopher Hoffman <CSH.Hoffman@gmail.com>
2026-04-27 14:14:57 -0400

    feat: add AddElectricityCostTracking migration

---
+++ PrintLogApi/Migrations/20260427181240_AddElectricityCostTracking.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrintLogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddElectricityCostTracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "WattageW",
+                table: "Printers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "UserSettingTypes",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 12, "The user's electricity rate in currency per kWh.", "Electricity_KwhRate" },
+                    { 13, "Default printer wattage in watts used when a printer has no specific wattage set.", "Electricity_DefaultWattageW" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WattageW",
+                table: "Printers");
+
+            migrationBuilder.DeleteData(
+                table: "UserSettingTypes",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "UserSettingTypes",
+                keyColumn: "Id",
+                keyValue: 13);
+        }
+    }
+}
